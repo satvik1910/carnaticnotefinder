57045c092f857c1b161abb407e175f08648a3aa2
satvik1910 <satvik1910@gmail.com>
2025-08-08 12:07:51 -0700

    Fix Gunicorn configuration for production deployment

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,9 @@ WSGI config for RagaNoteFinder.
 This module contains the WSGI application used by the application server.
 """
 
-from app import app as application
+from app import create_app
+
+application = create_app()
 
 if __name__ == "__main__":
     application.run()
